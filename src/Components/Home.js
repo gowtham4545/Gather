687d2d5f32699b59b0fc9dde0f5a37083cf4ef3c
gowtham4545 <gowthamgopi444@gmail.com>
2022-12-07 13:48:06 +0530

    Homepage

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,17 +1,22 @@
 import React from 'react';
 import img1 from './pexels-fauxels-3183183.jpg';
+import img2 from './pexels-fauxels-3184287.jpg';
 import { Box, Image, Text } from '@chakra-ui/react'
 import { Container } from '@chakra-ui/react';
 
 export default function Home() {
     return (
-        <div>
-            <Box boxSize='xxl'>
-                <Image src={img1} alt='lkjll' />
-                <Container top={'40vh'} minW={'container.xl'} zIndex={2} position='absolute' centerContent >
-                    <Text fontSize='5xl' color={'black'}>Let's Gather</Text>
-                </Container>
+        <div id='body' style={{ height: '100vh', width: '100vw' }}>
+            <Box boxSize='sm' top={'300'} borderRadius='xl' right='500' position={'absolute'} height='auto' >
+                <Image className='homeImage' src={img1} alt='lkjll' border={'3px white'} borderRadius={'xl'} />
             </Box>
+            <Box boxSize='sm' top={'40px'} borderRadius='xl' right='10' position={'absolute'} height='auto' >
+                <Image className='homeImage' src={img2} alt='lkjll' border={'3px white'} borderRadius={'xl'} />
+            </Box>
+            <Container top={'20vh'} minW={'container.xl'} width={'auto'} zIndex={2} position='absolute' left={ '10'} >
+                <Text className='font-sans title' textShadow={'1px 1px #ffffff'} fontSize='8xl' color={'aliceblue'} >Let's Gather</Text>
+                <Text className='font-sans title' fontFamily={'mono'} textShadow={'1px 1px #ffffff'} fontSize='3xl' color={'aliceblue'} >One platform to connect</Text>
+            </Container>
 
         </div>
     )
